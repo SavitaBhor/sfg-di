5ded9d9bdb0cac4246903e0bb0fdfb9866260c50
SavitaBhor <67609286+SavitaBhor@users.noreply.github.com>
2021-03-25 22:49:05 -0400

    DI injection 3 way

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi;
 
 import guru.springframework.sfgdi.controller.MyController;
+import guru.springframework.sfgdi.controller.PropertyInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +16,10 @@ public class SfgDiApplication {
 
 		String greetings = myController.sayHello();
 		System.out.println(greetings);
+
+		System.out.println("------ Property");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
 	}
 
 }
